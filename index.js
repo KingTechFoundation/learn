007f018275ecaa4314dcbc6ac44b500b371e7668
KingTechFoundation <muhirehillary459@gmail.com>
2024-12-05 22:41:38 +0300

    Created users table and connected to MySQL database

--- index.js
+++ index.js
@@ -1,14 +1,17 @@
 const express = require('express');
-const db = require('./config/db');
+const db = require('./config/db'); // MySQL connection
 const App = express();
 const cors = require('cors');
 const userRoutes = require('./routes/userRoutes');
-const Port = 5000;
+const Port = process.env.PORT || 5000; // Set a default port if not in .env
+
+require('dotenv').config(); // Load environment variables
 
 App.use(express.json());
 App.use(cors());
-//routes
-App.use('/api/users', userRoutes);
+
+// Routes
+App.use('/api/users', userRoutes); // Attach the user routes
 
 App.listen(Port, () => {
   console.log(`Server listening on port ${Port}`);
